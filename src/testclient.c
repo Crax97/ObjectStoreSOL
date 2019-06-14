10bc75bdc1cb482f84b03fd03ffb4924f2814590
Crax <gsolimeno97@gmail.com>
2019-06-14 12:03:27 +0200

    Better tests

--- src/testclient.c
+++ src/testclient.c
@@ -5,27 +5,19 @@
 #include "libobjstore.h"
 
 #define EXITTHESHIP(msg) { perror(msg); exit(EXIT_FAILURE); }
+#define ASSERT(expr) if(!(expr)) { EXITTHESHIP( "Assert failed: " #expr "\n");}
 int main(int argc, char** argv) {
 	char test[] = "Hey, vsauce! Michael here!";
 	char name[] = "TestClient";	
 	char store_name[] = "vsauce";
-	if(os_connect(name) != 0) {
-		EXITTHESHIP("Connection failed");
-	}
+	ASSERT(os_connect(name) == 0); 
 
-	if(os_store(store_name, test, strlen(test)) != OS_OK) {
-		EXITTHESHIP("Store failed");
-	}
-
+	ASSERT(os_store(store_name, test, strlen(test)) == OS_OK); 
+	
 	char* retrieve = (char*)os_retrieve(store_name);
-	if(retrieve == NULL) {
-		EXITTHESHIP("Retrieve failed");
-	}
+	ASSERT(retrieve != NULL); 
+	ASSERT(strcmp(retrieve, test) == 0);
 
-	if(os_delete(store_name) != OS_OK) {
-		EXITTHESHIP("Delete failed");
-	}
-	if(os_disconnect() != OS_OK) {
-		EXITTHESHIP("Disconnect failed");
-	}
+	ASSERT(os_delete(store_name) == OS_OK);
+	ASSERT(os_disconnect() == OS_OK);
 }
